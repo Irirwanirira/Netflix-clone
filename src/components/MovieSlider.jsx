312dc19dfcbab775f755e7e97609213efb786596
Irirwanirira <irijoseph41@gmail.com>
2023-08-14 18:36:15 +0200

    Implement slide on the home screen

--- src/components/MovieSlider.jsx
+++ src/components/MovieSlider.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Card from './Card';
 import styled from 'styled-components';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
-export default React.memo(function MovieSlider({ data, title }){
+export default React.memo(function MovieSlider({ data, title }) {
+  const [ControlVisibility, setControlVisibility] = useState(false);
+
+  const listRef = useRef();
+  const [sliderPosition, setSliderPosition] = useState(0);
+
+  const handleDirection = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 70;
+    console.log(distance);
+    if (direction === 'left' && sliderPosition > 0) {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      setSliderPosition(sliderPosition - 1);
+    }
+    if (direction === 'right' && sliderPosition < 4) {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      setSliderPosition(sliderPosition + 1);
+    }
+  };
+
   return (
-    <Container>
+    <Container
+      ControlVisibility={ControlVisibility}
+      onMouseEnter={() => setControlVisibility(true)}
+      onMouseLeave={() => setControlVisibility(false)}
+    >
       <h1>{title}</h1>
       <div className='wrapper'>
-        <div className='slider'>
+        <div
+          className={`slider-action left ${!ControlVisibility ? 'none' : ''}`}
+        >
+          <AiOutlineLeft onClick={() => handleDirection('left')} />
+        </div>
+        <div
+          className='slider'
+          ref={listRef}
+        >
           {data.map((movie, index) => {
             return (
               <Card
@@ -18,30 +49,65 @@ export default React.memo(function MovieSlider({ data, title }){
             );
           })}
         </div>
+        <div
+          className={`slider-action right ${!ControlVisibility ? 'none' : ''}`}
+        >
+          <AiOutlineRight onClick={() => handleDirection('right')} />
+        </div>
       </div>
     </Container>
   );
 });
 
 const Container = styled.div`
-    gap: 0.7rem;
-    position: relative;
-    padding: 1rem 0;
-    h1{
-        color:white;
-        margin-left: 5px;
-        font-size: 25px;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  gap: 0.7rem;
+  position: relative;
+  padding: 1rem 0;
+  h1 {
+    color: white;
+    margin-left: 5px;
+    font-size: 25px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+
+  .wrapper {
+    .slider {
+      display: flex;
+      width: max-content;
+      gap: 0.6rem;
+      transform: translateX(0px);
+      transition: 1s ease-in-out;
+      margin-left: 5px;
     }
 
-    .wrapper{
-        .slider{
-            display: flex;
-            width:max-content;
-            gap: 0.6rem;
-            transform:  translateX(0px);
-            transition: 1s ease-in-out;
-            margin-left: 5PX;
-        }
+    .slider-action {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      z-index: 99;
+      height: 100%;
+      top: 2rem;
+      bottom: 0;
+      width: 50px;
+      transition: 0.1s ease-in-out;
+      svg {
+        background-color: #2f2e2e;
+        border-radius: 50%;
+        padding: 0.2rem;
+        font-size: 2rem;
+        cursor: pointer;
+        color: white;
+      }
     }
+    .left {
+      left: 0;
+    }
+    .right {
+      right: 0;
+    }
+    .none {
+      display: none;
+    }
+  }
 `;
